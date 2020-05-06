3c8aac593f366ae8c6ea6cb7661a6caaa9d3d3a3
YunaH <YunaH503@gmail.com>
2020-05-05 20:06:09 -0400

    Fixed results to display questions as a list

--- js/quiz.js
+++ js/quiz.js
@@ -8,32 +8,50 @@ const quiz = [
 let correctAnswer = []
 let wrongAnswer = []
 
+let question;
+let answer;
+let response;
+let html;
+
+// Prints the message onto the HTML page
 function print(message) {
-  document.write(message);
+  let outputDiv = document.getElementById('output');
+  outputDiv.innerHTML = message;
+}
+
+// Function that stores the list of answered questions in an array
+function list(arr) {
+  let listHTML = '<ol>'
+    for (var i = 0; i < arr.length; i++) {
+      listHTML += `<li> ${arr[i]} </li>`
+    }
+  listHTML += '</ol>'
+  return listHTML
 }
 
 // 2. Add a loop to cycle through the array, ask and compare the users response to the answers in the array.
 for(let i = 0; i < quiz.length; i++) {
   // 3. Prompt the user to answer the questions.
-  let answer = prompt(quiz[i][0])
+  answer = prompt(quiz[i][0])
+  question = quiz[i][0]
   // 4. Use a conditional statement to compare users response to the correct answers.
   if( answer.toLowerCase() === quiz[i][1]) {
-    correctAnswer.push(quiz[i][0])
+    correctAnswer.push(question)
   } else {
-    wrongAnswer.push(quiz[i][0])
+    wrongAnswer.push(question)
   }
 }
 
 // 5. Print the number of correct & incorrect answers to the page
 function results() {
-  let html = `<p>You got ${correctAnswer.length} questions right!</p>`
+  html = `<p>You got ${correctAnswer.length} questions right and ${wrongAnswer.length} questions wrong.</p>`
 
-  if( wrongAnswer.length > 0 ) {
-    html = `<p>You got ${wrongAnswer.length} questions wrong.</p>`
-    return html
-  } else {
-    return html
-  }
+  html += `<p>Questions you got correct: </p>`
+  html += list(correctAnswer)
+
+  html += `<p>Questions you got wrong:</p>`
+  html += list(wrongAnswer)
+  return html
 }
 
-results()
+print(results() )
